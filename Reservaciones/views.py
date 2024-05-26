1d56efdbffe09424d51e1715c2092fd8253ffdf4
SH4D0WKNIGHT <danyhalomadero@gmail.com>
2024-05-25 22:48:54 -0600

    Creación del login y el registro para los usuarios. Así como de algunas vistas.

--- Reservaciones/views.py
+++ Reservaciones/views.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
 def main(request):
-    render(request, 'base.html')
+    return render(request, 'base.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {'form': UserCreationForm, 'prompt': 'Username already exists'})
+        return render(request, 'signup.html', {'form': UserCreationForm, 'prompt': 'Passwords do not match'})
+        
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+@login_required
+def tasks(request):
+    return render(request, 'tasks.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('index')
