1341978e7ad57b35b9ddf5d34efef05ce15f2646
LOBOANONIMO <l0b04n0n1m0@gmail.com>
2024-05-28 00:09:08 -0600

    Eliminación de llave foránea.

--- usuarios_hoteles.sql
+++ usuarios_hoteles.sql
@@ -309,8 +309,7 @@ CREATE TABLE `reservaciones` (
   `lugar` tinytext,
   `fecha` datetime DEFAULT NULL,
   `numero_tarjeta` varchar(16) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `reservaciones_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
